e3dbf4e25a716155af53adf8b28c1b6b8bb4b9e9
Hamderber <kevinscoursey@gmail.com>
2023-10-17 13:29:48 -0700

    Character controller: Movement and camera

--- Code/Character/Player/player.gd
+++ Code/Character/Player/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+# Important note: The scale of the player is 2! This means that the normal things like gravity
+# doubled
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -600.0
 # Step height in pixels
-const STEP_HEIGHT = 5
+const STEP_HEIGHT = 10
 # Nodes to get at runtime:
 @onready var animation_player := $AnimationPlayer
 @onready var animated_sprite_2d := $AnimatedSprite2D
@@ -27,7 +30,7 @@ func _physics_process(delta):
 	# Add the GRAVITY, but only if the player is in the air.
 	if not is_on_floor():
 		# Applying 75% of gravity to the player gives it a nice feel
-		velocity.y += GRAVITY * 0.75 * delta
+		velocity.y += GRAVITY * 1.5 * delta
 	else:
 		double_jumps_used = 0
 
@@ -65,6 +68,6 @@ func _position_raycasts():
 	# Gets the CollisionShape2D's height (y)
 	var collision_shape_height = $CollisionShape2D.shape.size.y
 	# Positions the RayCast2Ds to function as "feet"
-	raycast2d_front.transform.origin = Vector2(STEP_HEIGHT, 0.5 * collision_shape_height - STEP_HEIGHT + 1)
-	raycast2d_back.transform.origin = Vector2(-STEP_HEIGHT, 0.5 * collision_shape_height - STEP_HEIGHT + 1)
+	raycast2d_front.transform.origin = Vector2(STEP_HEIGHT, 0.5 * collision_shape_height - STEP_HEIGHT + 2)
+	raycast2d_back.transform.origin = Vector2(-STEP_HEIGHT, 0.5 * collision_shape_height - STEP_HEIGHT + 2)
 	
