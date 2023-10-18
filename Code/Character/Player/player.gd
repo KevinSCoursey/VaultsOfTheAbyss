dd4af8bd45c331da57d12ef5a5683205051152a7
Hamderber <kevinscoursey@gmail.com>
2023-10-18 11:01:25 -0700

    Sprite2D vice AnimatedSprite2D

--- Code/Character/Player/player.gd
+++ Code/Character/Player/player.gd
@@ -10,6 +10,7 @@ const STEP_HEIGHT = 10
 # Nodes to get at runtime:
 @onready var animation_player := $AnimationPlayer
 @onready var animated_sprite_2d := $AnimatedSprite2D
+@onready var animation_tree := $AnimationTree
 # Get the auto-loaded resource UserInputConstants from the scene root. This is a constant,
 # but cannot be assigned at runtime if it is defined as a const.
 @onready var USER_INPUT := $"/root/UserInputConstants"
@@ -22,9 +23,19 @@ const STEP_HEIGHT = 10
 # Controls the number of multi-jumps the player has available
 var double_jumps_max := 2
 var double_jumps_used := 0
+# Parameters used to track the states/properties of the player
+var is_melee_attacking = false
+var is_running = false
+var is_jumping = false
+var is_multi_jumping = false
 
 func _ready():
-	_position_raycasts()
+	animation_tree.active = true
+	position_raycasts()
+
+func _process(_delta):
+	pass
+	# update_animation_parameters()
 
 func _physics_process(delta):
 	# Add the GRAVITY, but only if the player is in the air.
@@ -41,9 +52,9 @@ func _physics_process(delta):
 		# If this is the first jump, use the jump animation.
 		# Otherwise, it is a multi-jump, and should use that animation instead.
 		if double_jumps_used == 0:
-			animation_player.play("jump")
+			$AnimationTree.get("parameters/playback").travel("jump")
 		else:
-			animation_player.play("multi-jump")
+			$AnimationTree.get("parameters/playback").travel("multi-jump")
 		velocity.y = JUMP_VELOCITY
 		double_jumps_used += 1
 
@@ -52,35 +63,38 @@ func _physics_process(delta):
 	var direction = Input.get_axis(USER_INPUT.MOVE_LEFT, USER_INPUT.MOVE_RIGHT)
 	if direction:
 		if velocity.y == 0:
-			animation_player.play("run")
+			$AnimationTree.get("parameters/playback").travel("run")
 		velocity.x = direction * SPEED
 		animated_sprite_2d.flip_h = direction < 0
 	else:
-		if velocity.y == 0 and not \
-		(animation_player.is_playing() and animation_player.current_animation == "melee-attack-ground"):
-			animation_player.play("idle")
+		if velocity.y == 0 and is_on_floor():
+			$AnimationTree.get("parameters/playback").travel("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if velocity.y > 0 and not is_on_floor() and not \
-	(animation_player.is_playing() and animation_player.current_animation == "multi-jump" \
-	or animation_player.current_animation == "melee-attack-air"):
-		animation_player.play("fall")
+	if velocity.y > 0 and not is_on_floor():
+		$AnimationTree.get("parameters/playback").travel("fall")
 	move_and_slide()
 	
 func _unhandled_input(event):
 	if event.is_action_pressed("attack"):
 		# Makes the player look in the direction that the mouse was clicked for an attack
-		animated_sprite_2d.flip_h = not _is_mouse_right_of_player()
+		animated_sprite_2d.flip_h = not is_mouse_right_of_player()
 		# Play the appropriate attack animation depending on if the player is on the ground or not
 		if is_on_floor():
-			animation_player.play("melee-attack-ground")
+			$AnimationTree.get("parameters/playback").travel("melee-attack-ground")
 		else:
-			animation_player.play("melee-attack-air")
-		
-func _is_mouse_right_of_player():
+			$AnimationTree.get("parameters/playback").travel("melee-attack-air")
+
+func update_animation_parameters():
+	animation_tree["parameters/conditions/is_melee_attacking"] = is_melee_attacking
+	animation_tree["parameters/conditions/is_jumping"] = is_jumping
+	animation_tree["parameters/conditions/is_running"] = is_running
+	animation_tree["parameters/conditions/is_on_floor"] = is_on_floor()
+	
+func is_mouse_right_of_player():
 	# Checks if the position of the mouse is to the right of the player
 	return get_global_mouse_position().x >= transform.origin.x
 
-func _position_raycasts():
+func position_raycasts():
 	# Gets the CollisionShape2D's height (y)
 	var collision_shape_height = $CollisionShape2D.shape.size.y
 	# Positions the RayCast2Ds to function as "feet"
